11fe8b212c66c25b5b5f5d5b279f4c2906289978
donghwan <wkazxf@gmail.com>
2022-02-27 13:40:35 +0900

    initial commit

--- src/main/java/com/hwanld/EntripAPI/EntripApiApplication.java
+++ src/main/java/com/hwanld/EntripAPI/EntripApiApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EntripApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EntripApiApplication.class, args);
+
 	}
 
 }
