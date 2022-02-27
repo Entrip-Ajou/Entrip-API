fa9ca10303b53e9976be820cc4d1d26aa808d43e
donghwan <wkazxf@gmail.com>
2022-02-27 15:15:00 +0900

    lombok,junit

--- src/main/java/com/hwanld/EntripAPI/EntripApiApplication.java
+++ src/main/java/com/hwanld/EntripAPI/EntripApiApplication.java
@@ -5,11 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EntripApiApplication {
-
 	public static void main(String[] args) {
-
 		SpringApplication.run(EntripApiApplication.class, args);
-
 	}
-
 }
