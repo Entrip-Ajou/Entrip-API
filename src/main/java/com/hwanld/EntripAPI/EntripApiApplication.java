db5dc7b4195188ac4a2f491f421f988c457f1980
donghwan <wkazxf@gmail.com>
2022-02-27 13:38:55 +0900

    Initial commit

---
+++ src/main/java/com/hwanld/EntripAPI/EntripApiApplication.java
@@ -0,0 +1,13 @@
+package com.hwanld.EntripAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntripApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntripApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hwanld/EntripAPI/EntripApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hwanld.EntripAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntripApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
