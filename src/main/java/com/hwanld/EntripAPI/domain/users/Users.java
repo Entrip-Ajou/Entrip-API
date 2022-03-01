a2ef4a6bb3076bcbdddacb90c46e2871394a6518
donghwan <wkazxf@gmail.com>
2022-03-01 12:08:32 +0900

    Plan Entity 클래스 추가

---
+++ src/main/java/com/hwanld/EntripAPI/domain/users/Users.java
@@ -0,0 +1,15 @@
+//package com.hwanld.EntripAPI.domain.users;
+//
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//
+//@Getter
+//@NoArgsConstructor
+//
+//public class Users {
+//    private String id;
+//    private String name;
+//    private String picture;
+//    private String email;
+//}
