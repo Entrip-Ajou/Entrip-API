ab6ea7cd44f7e2c3ab62960b8876074a49f34f60
donghwan <wkazxf@gmail.com>
2022-06-10 01:16:53 +0900

    TravelSpotsResponseDto

--- src/main/java/com/hwanld/EntripAPI/web/dto/travelSpots/TravelSpotsResponseDto.java
+++ src/main/java/com/hwanld/EntripAPI/web/dto/travelSpots/TravelSpotsResponseDto.java
@@ -3,20 +3,32 @@ package com.hwanld.EntripAPI.web.dto.travelSpots;
 import com.hwanld.EntripAPI.domain.travelSpots.TravelSpots;
 import lombok.Getter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Getter
 public class TravelSpotsResponseDto {
 
-    private String travelSpot_name;
+    private String name;
     private String photoUrl;
-    private String location;
-    private String description;
-    private String travelFavorite;
+    private String address;
+    private List<String> tags;
 
     public TravelSpotsResponseDto (TravelSpots entity) {
-        this.travelFavorite = entity.getTravelFavorite();
+        this.name = entity.getTravelFavorite();
         this.photoUrl = entity.getPhotoUrl();
-        this.location = entity.getLocation();
-        this.travelSpot_name = entity.getTravelSpot_name();
-        this.description = entity.getDescription();
+        this.address = entity.getLocation();
+        this.tags = new ArrayList<String>();
+        this.tags = makeStringToList(entity.getTravelFavorite());
     }
+
+    public List<String> makeStringToList (String travelFavorite) {
+        List<String> tags = new ArrayList<String>();
+        for (int i=0;i<travelFavorite.length();i++) {
+            String tempString = travelFavorite.substring(i,i+1);
+            tags.add(tempString);
+        }
+        return tags;
+    }
+
 }
